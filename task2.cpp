006436c170fe796da0b80d3fdb7d5b149d5755d0
RaflyArif <128361879+RaflyArif@users.noreply.github.com>
2023-05-09 21:42:03 +0700

    Update task2.cpp

--- task2.cpp
+++ task2.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
+
 using namespace std;
 
-int main() {
-    long int nb_input, nb_output;
-    cout<<"INPUT : ";
-    cin>>nb_input;
-    while(nb_input > 0){
-        int a = (nb_input % 10);
-        nb_output = nb_output + a;
-        nb_input /= 10;
+int countFrequency(int arr[], int n, int x) {
+    int frequency = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == x) {
+            frequency++;
+        }
     }
-    cout<<endl<<"OUTPUT : "<<nb_output<<endl;
+    return frequency;
+}
+
+int main() {
+    int arr[] = {1, 2, 2, 3, 3, 3, 4, 4, 4, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x = 3;
+    int frequency = countFrequency(arr, n, x);
+    cout << "Frekuensi angka " << x << " adalah " << frequency << endl;
     return 0;
 }
